4e7330f3112a4eb79eea1092ef537bc8210ea4c7
SkyLight <35933068+SkyLight2000@users.noreply.github.com>
2018-06-03 12:19:57 +0300

    Add files via upload

---
+++ funcs.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "Complex.h"
+#include "funcs.h"
+
+Complex a(5, 2);
+Complex b(3, -3);
+
+int tab_handler1(void* p) {
+	return view();
+}
+
+int tab_handler2(void* p) {
+	return sum();
+}
+
+int tab_handler3(void* p) {
+	return sub();
+}
+
+int tab_handler4(void* p) {
+	return mult();
+}
+
+int tab_handler5(void* p) {
+	return exit();
+}
+
+int view() {
+	cout << "a = " << a << "; b =" << b << endl;
+	return 0;
+}
+
+int sum() {
+	cout << a + b << endl;
+	return 0;
+}
+
+int sub() {
+	cout << a - b << endl;
+	return 0;
+}
+
+int mult() {
+	cout << a * b << endl;
+	return 0;
+}
+
+int exit() {
+	exit(EXIT_SUCCESS);
+}
---
+++ funcs.h
@@ -0,0 +1,13 @@
+#pragma once
+typedef int(*handler_t)(void*);
+int menu(const char*[]);
+int tab_handler1(void* p);
+int tab_handler2(void* p);
+int tab_handler3(void* p);
+int tab_handler4(void* p);
+int tab_handler5(void* p);
+int view();
+int sum();
+int sub();
+int mult();
+int exit();
