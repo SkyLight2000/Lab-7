febb3f46f4688adb527b0a421f2da2aa5ebddc9a
SkyLight <35933068+SkyLight2000@users.noreply.github.com>
2018-06-03 12:20:47 +0300

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -1,16 +1,4 @@
 #pragma once
-typedef int(*handler_t)(void*);
-int menu(const char*[]);
-int tab_handler1(void* p);
-int tab_handler2(void* p);
-int tab_handler3(void* p);
-int tab_handler4(void* p);
-int tab_handler5(void* p);
-int view();
-int sum();
-int sub();
-int mult();
-int exit();
 
 class Complex {
 private:
