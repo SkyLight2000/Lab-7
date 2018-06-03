6dafa8c08232e1feb900e09750f7d7aa42ac273c
SkyLight <35933068+SkyLight2000@users.noreply.github.com>
2018-06-03 12:20:28 +0300

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,9 +1,6 @@
 #include "stdafx.h"
 #include "Complex.h"
 
-Complex a(5, 2);
-Complex b(3, -3);
-
 Complex::Complex(double r, double i) {
 	re = r;
 	im = i;
@@ -22,48 +19,4 @@ Complex Complex::operator * (const Complex &c) {
 ostream& operator<< (ostream &out, const Complex &c) {
 	out << "(" << c.re << ", " << c.im << ")";
 	return out;
-}
-
-int tab_handler1(void* p) {
-	return view();
-}
-
-int tab_handler2(void* p) {
-	return sum();
-}
-
-int tab_handler3(void* p) {
-	return sub();
-}
-
-int tab_handler4(void* p) {
-	return mult();
-}
-
-int tab_handler5(void* p) {
-	return exit();
-}
-
-int view() {
-	cout << "a = " << a << "; b =" << b << endl;
-	return 0;
-}
-
-int sum() {
-	cout << a + b << endl;
-	return 0;
-}
-
-int sub() {
-	cout << a - b << endl;
-	return 0;
-}
-
-int mult() {
-	cout << a * b << endl;
-	return 0;
-}
-
-int exit() {
-	exit(EXIT_SUCCESS);
 }
